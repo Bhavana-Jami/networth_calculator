46ddf22280d20c8ef6f33baef9b66d389997e40f
Bhavana Jami <bhavanajami111@gmail.com>
2024-04-20 23:44:33 +0530

    basic template

--- src/components/AccordionComponent.js
+++ src/components/AccordionComponent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Accordion from '@mui/material/Accordion';
 import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -6,8 +6,30 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
 import SliderComponent from './SliderComponent';
+import { Box, FormControl, InputLabel, OutlinedInput, InputAdornment, Slider } from '@mui/material'
+
 
 function AccordionComponent(props) {
+
+
+    // const [realEstateInputValue, setRealEstateInputValue] = useState(500)
+
+    // const handleInputChange = () => {
+
+    // }
+    // const handlePersonalInputChange = () => {
+
+    // }
+    // const handleRealInputChange = (e) => {
+    //     realEstateInputValue(e.target.value)
+
+    // }
+    // const yourAssets =
+    //     [{ id: 1, inputTitle: "Real estate", value: 1000, onChange: (event) => handleRealInputChange(1, event), state: realEstateInputValue },
+    //     { id: 2, inputTitle: "Personal property", value: 100, onChange: (event) => handlePersonalInputChange(2, event) },
+    //     { id: 3, inputTitle: "Investments", value: 100, onChange: (event) => handleInputChange(3, event) },
+    //     { id: 4, inputTitle: "Cash", value: 100, onChange: (event) => handleInputChange(4, event) }]
+
     return (
         <Accordion>
             <AccordionSummary
@@ -19,10 +41,34 @@ function AccordionComponent(props) {
                 {props.title}
             </AccordionSummary>
             <AccordionDetails>
-                {props.title === "Total Assets" ?
-                    ["asd", "asdf"].map((data) => <SliderComponent inputTitle={data} />):
-                    ["bha", "asdfsadfdff"].map((data) => <SliderComponent inputTitle={data} />)
-            }
+                {
+                    props.title === "Total Assets" ?
+                        [{ inputTitle: "Real estate", value: 100000 },
+                        { inputTitle: "Personal property", value: 100 },
+                        { inputTitle: "Investments", value: 100 },
+                        { inputTitle: "Cash", value: 100 }].map((data) => <SliderComponent inputTitle={data.inputTitle} value={data.value} />) :
+                        [{ inputTitle: "Mortagage balance", value: 100 },
+                        { inputTitle: "Loan balaces", value: 100 },
+                        { inputTitle: "Credit card debt", value: 100 }
+                        ].map((data) => <SliderComponent inputTitle={data.inputTitle} value={data.value} />)
+                }
+                {/* {
+                    props.title === "Total Assets" ?
+                        yourAssets.map((data) => (
+                            <Box>
+                                <FormControl fullWidth sx={{ m: 1 }}>
+                                    <InputLabel htmlFor="outlined-adornment-amount">{data.inputTitle}</InputLabel>
+                                    <OutlinedInput
+                                        id="outlined-adornment-amount"
+                                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                        label="Amount"
+                                        value={data.state}
+
+                                    />
+                                </FormControl>
+                                <Slider defaultValue={0} aria-label="Default" valueLabelDisplay="auto" min={0} max={1000} onChange={data.onChange} />
+                            </Box>)) : null
+                } */}
             </AccordionDetails>
 
 
--- src/components/SliderComponent.js
+++ src/components/SliderComponent.js
@@ -1,20 +1,26 @@
-import { Box, FormControl,InputLabel,OutlinedInput,InputAdornment, Slider} from '@mui/material'
-import React from 'react'
+import { Box, FormControl, InputLabel, OutlinedInput, InputAdornment, Slider } from '@mui/material'
+import React, { useState } from 'react'
 
 function SliderComponent(props) {
-  return (
-   <Box>
-     <FormControl fullWidth sx={{ m: 1 }}>
-          <InputLabel htmlFor="outlined-adornment-amount">{props.inputTitle}</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-amount"
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-            label="Amount"
-          />
-        </FormControl>
-        <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
-   </Box>
-  )
+    const [sliderValue, setSliderValue] = useState()
+    const handleSliderChange=(e)=>{
+        setSliderValue(e.target.value)
+        // props.sendSliderValueToParent(sliderValue)
+    }
+    return (
+        <Box>
+            <FormControl fullWidth sx={{ m: 1 }}>
+                <InputLabel htmlFor="outlined-adornment-amount">{props.inputTitle}</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-amount"
+                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                    label="Amount"
+                    value={props.value}
+                />
+            </FormControl>
+            <Slider defaultValue={props.value} aria-label="Default" valueLabelDisplay="auto" min={0} max={1000} onChange={handleSliderChange} />
+        </Box>
+    )
 }
 
 export default SliderComponent
